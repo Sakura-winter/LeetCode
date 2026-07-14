79a93a0e0f023f37ab242ed6856085abfa5ab6d1
Sumit Kumar Mehta <Sumitkum1819@gmail.com>
2026-07-14 12:58:17 +0530

    LC-remove-outermost-parentheses [Runtime: N/A; Memory: N/A]

--- stack/remove-outermost-parentheses.cpp
+++ stack/remove-outermost-parentheses.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     string removeOuterParentheses(string s) {
-        string ans = "";
+        /*string ans = "";
         stack<char>ss;
         for(char c: s){
             if(c == '('){
@@ -21,6 +21,30 @@ public:
                         ans +=c;
                     }
                 }
+            }
+        }
+        return ans;*/
+
+
+        //doing using count variable, if '(' count + 1, else -1;
+        int cnt = 0;
+        string ans = "";
+        for(char c: s){
+            if(c == '('){
+                if(cnt == 0){
+                    cnt++;
+                }else{
+                    cnt++;
+                    ans +=c;
+                }
+                //cnt++;
+            }else{
+
+                cnt--;
+                if(cnt > 0){
+                    ans +=c;
+                }
+
             }
         }
         return ans;
