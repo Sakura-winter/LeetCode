1de07349a596f73f98e8eb4a2b1ee56556b280fc
Sumit Kumar Mehta <Sumitkum1819@gmail.com>
2026-07-16 09:16:09 +0530

    LC-remove-all-adjacent-duplicates-in-string [Runtime: N/A; Memory: N/A]

--- string/remove-all-adjacent-duplicates-in-string.cpp
+++ string/remove-all-adjacent-duplicates-in-string.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     string removeDuplicates(string s) {
-        stack<char> st;
+        /*stack<char> st;
         for(char c : s){
             if(st.empty()){
                 st.push(c);
@@ -20,6 +20,30 @@ public:
         }
         
         reverse(ans.begin(), ans.end());
-        return ans;
+        return ans;*/
+//--------------------------------------------------------------
+        //not using stack, only using string
+        /*string ans = "";
+        for(int c : s){
+            if(!ans.empty() && ans.back() == c){
+                ans.pop_back();
+            }else{
+                ans.push_back(c);
+            }
+        }
+        return ans;*/
+//--------------------------------------------------------------------
+        //using original string as answer
+        int i = 0;
+        for(char c : s){
+            s[i] = c;
+            if(i > 0 && s[i] == s[i - 1]){
+                i-=2;
+            }
+            i++;
+        }
+        return s.substr(0, i);
+        //T.C = O(n) and S.C = O(1);
+
     }
 };
