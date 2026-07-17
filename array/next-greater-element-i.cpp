1977d9031829773b068a42820c0c2cdc000e3ef8
Sumit Kumar Mehta <Sumitkum1819@gmail.com>
2026-07-17 14:11:40 +0530

    LC-next-greater-element-i [Runtime: N/A; Memory: N/A]

--- array/next-greater-element-i.cpp
+++ array/next-greater-element-i.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
-        vector<int> ans;
+        /*vector<int> ans;
         
         for(int i = 0; i < nums1.size(); i++){
             bool flag = false;
@@ -19,6 +19,21 @@ public:
                    
             
         }
+        return ans;*/
+        unordered_map<int,int> mp;
+        stack<int> st;
+        for(int i = nums2.size()-1; i >= 0; i--){
+            while(!st.empty() && st.top() <= nums2[i]){
+                st.pop();
+            }
+            mp[nums2[i]] = st.empty()? -1 : st.top();
+            st.push(nums2[i]);
+        }
+        vector<int> ans;
+        for(int i: nums1){
+            ans.push_back(mp[i]);
+        }
         return ans;
+        
     }
 };
